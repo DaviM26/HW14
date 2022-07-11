125032cacade35c953603ddfc8fb1df854e5a4c7
DaviM26 <100162433+DaviM26@users.noreply.github.com>
2022-07-11 14:51:34 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='Demo',
       version='0.1',
       description='A pointless demonstration package',
-      url='http://github.com/storborg/m14-demo',
+      url='https://github.com/DaviM26/HW14/edit/mainsetup.py',
       author='DaviM26',
       #author_email='',
       license='GNU',
