bb4652d0dca8c8224408ceb0036a5142ca5c6d26
DaviM26 <100162433+DaviM26@users.noreply.github.com>
2022-07-11 14:52:13 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-setup(name='Demo',
-      version='0.1',
-      description='A pointless demonstration package',
-      url='https://github.com/DaviM26/HW14/edit/mainsetup.py',
+setup(name='HW14',
+      #version='0.1',
+      description='hw14',
+      url='https://github.com/DaviM26/HW14',
       author='DaviM26',
       #author_email='',
       license='GNU',
